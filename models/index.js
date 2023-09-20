b4ae99ee626a7f221c210d1e1ee644b9393ce4bc
Nick Burr <nicholasstclairburr@gmail.com>
2023-09-20 01:12:00 -0600

    Added in the 2 other models, and the relations in the index.js file. now need routes.

--- models/index.js
+++ models/index.js
@@ -13,8 +13,20 @@ Category.hasMany(Product, {
   foreignKey: 'category_id'
 })
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+})
 
 module.exports = {
   Product,
